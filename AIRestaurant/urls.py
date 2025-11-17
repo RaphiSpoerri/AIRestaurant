c5357cb7998beb8bedb022abe45b88dbc1842b11
Raphael Spoerri <coolrustacean@gmail.com>
2025-11-17 17:12:58 -0500

    fixed renaming

--- AIRestaurant/urls.py
+++ AIRestaurant/urls.py
@@ -22,4 +22,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path("admin/", admin.site.urls),
+    path('index/', views.home),
+    path('add_to_cart', views.add_to_cart)
 ]
--- AIRestaurant/views.py
+++ AIRestaurant/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def add_to_cart(request):
+    return render(request,'cart.html')
